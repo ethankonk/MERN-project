1b6c9c8c98dba38602cb82ef7deb0ab9f94ab365
ethan.konk <ethan.konkolowicz@gmail.com>
2023-10-29 13:01:26 -0400

    added delete workouts function

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,6 +1,9 @@
 import { useState } from "react"
+import { useWorkoutContext } from "../hooks/useWorkoutContext"
 
 const WorkoutForm = () => {
+    const { dispatch } = useWorkoutContext()
+
     const [title, setTitle] = useState('')
     const [load, setLoad] = useState('')
     const [reps, setReps] = useState('')
@@ -29,6 +32,7 @@ const handleSubmit = async (e) => {
         setReps('')
         setError(null)
         console.log('new workout added', json)
+        dispatch({type: 'CREATE_WORKOUT', payload: json})
     }
 }
 
